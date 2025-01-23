021f6407b46511d4336cef61022f182519c3c189
Vinh Nguyen <ngtv0404@gmail.com>
2025-01-23 11:23:00 +1100

    implement robot control
    not really tested

--- central_nav/central_node.py
+++ central_nav/central_node.py
@@ -15,7 +15,66 @@ from visualization_msgs.msg import MarkerArray, Marker
 
 from typing_extensions import Self
 
-IX_RADIUS = 0.2 # intersection radius in metres
+IX_RADIUS = 0.3 # intersection radius in metres
+
+class Intersection:
+    def __init__(self, position: tuple[float, float], occupant: str | None = None):
+        self.position = position
+        self.occupant = occupant
+    
+    @property
+    def occupied(self) -> bool:
+        return self.occupant is not None
+    
+    def can_enter(self, robot: str) -> bool:
+        return self.occupant is None or self.occupant == robot # either unoccupied, or occupied by us
+
+    def enter(self, robot: str) -> bool: # returns whether robot can proceed
+        if self.can_enter(robot):
+            self.occupant = robot
+            return True
+        else:
+            return False
+    
+    def leave(self, robot: str):
+        if self.occupant == robot: # we're originally occupying the intersection
+            self.occupant = None
+    
+    def distance(self, position: tuple[float, float]) -> float:
+        x, y = position
+        x0, y0 = self.position
+        return ((x-x0)**2 + (y-y0)**2)**0.5
+    
+    def closest_intersection(self, intersections: list[Self]) -> tuple[int, Self]:
+        min_distance = float('inf')
+        min_ix = None
+        for (idx, ix) in enumerate(intersections):
+            distance = self.distance(ix.position)
+            if distance < min_distance:
+                min_distance = distance
+                min_ix = (idx, ix)
+        
+        return min_ix
+
+    @property
+    def marker(self):
+        marker = Marker()
+        # marker.header.stamp = self.get_clock().now().to_msg()
+        marker.header.frame_id = 'map'
+        marker.type = Marker.CYLINDER
+        marker.pose.position.x, marker.pose.position.y = self.position   
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = marker.scale.y = IX_RADIUS * 2; marker.scale.z = 0.01
+        marker.color.a = 1.0
+        if self.occupied:
+            marker.color.r = 1.0
+        else:
+            marker.color.g = 1.0
+        marker.frame_locked = True
+        return marker
+    
+    def __repr__(self) -> str:
+        return f'Intersection({self.position}, occupant: {self.occupant})'
 
 class Robot:
     def __init__(self, pose: TransformStamped | None = None, path: Path | None = None, min_path_length: float = 0.05):
@@ -80,67 +139,9 @@ class Robot:
     
     def __repr__(self) -> str:
         return f'Robot({self.position}, {len(self.waypoints)} wpt)'
-
-class Intersection:
-    def __init__(self, position: tuple[float, float], occupant: str | None = None):
-        self.position = position
-        self.occupant = occupant
     
-    @property
-    def occupied(self) -> bool:
-        return self.occupant is not None
-    
-    def can_enter(self, robot: str) -> bool:
-        return self.occupant is None or self.occupant == robot # either unoccupied, or occupied by us
-
-    def enter(self, robot: str) -> bool: # returns whether robot can proceed
-        if self.can_enter(robot):
-            self.occupant = robot
-            return True
-        else:
-            return False
-    
-    def leave(self, robot: str):
-        if self.occupant == robot: # we're originally occupying the intersection
-            self.occupant = None
-    
-    def distance(self, position: tuple[float, float]) -> float:
-        return self.distance(position[0], position[1])
-
-    def distance(self, x: float, y: float) -> float:
-        x0, y0 = self.position
-        return ((x-x0)**2 + (y-y0)**2)**0.5
-    
-    def closest_intersection(self, intersections: list[Self]) -> Self:
-        min_distance = float('inf')
-        min_ix = None
-        for ix in intersections:
-            distance = self.distance(ix.position)
-            if distance < min_distance:
-                min_distance = distance
-                min_ix = ix
-        
-        return min_ix
-
-    @property
-    def marker(self):
-        marker = Marker()
-        # marker.header.stamp = self.get_clock().now().to_msg()
-        marker.header.frame_id = 'map'
-        marker.type = Marker.CYLINDER
-        marker.pose.position.x, marker.pose.position.y = self.position   
-        marker.pose.orientation.w = 1.0
-        marker.scale.x = marker.scale.y = IX_RADIUS * 2; marker.scale.z = 0.01
-        marker.color.a = 1.0
-        if self.occupied:
-            marker.color.r = 1.0
-        else:
-            marker.color.g = 1.0
-        marker.frame_locked = True
-        return marker
-    
-    def __repr__(self) -> str:
-        return f'Intersection({self.position}, occupant: {self.occupant})'
+    def in_intersection(self, ix: Intersection) -> bool:
+        return ix.distance(self.position) <= 2 * IX_RADIUS
 
 class CentralNavigationNode(Node):
     def __init__(self):
@@ -154,7 +155,7 @@ class CentralNavigationNode(Node):
         self.create_subscription(TransformStamped, 'robot_poses', self.pose_cb, qos.qos_profile_system_default)
         self.create_subscription(Path, 'robot_paths', self.path_cb, qos.qos_profile_system_default)
 
-        self.collision_points: list[Intersection] = []
+        self.intersections: list[Intersection] = []
 
     def pose_cb(self, data: TransformStamped):
         robot_name = data.child_frame_id # as per pose_publisher node
@@ -162,6 +163,29 @@ class CentralNavigationNode(Node):
             self.robots[robot_name] = Robot(pose=data)
         else:
             self.robots[robot_name].set_pose(data)
+
+        self.check_intersections()
+    
+    def check_intersections(self):
+        for robot_name in self.robots:
+            robot = self.robots[robot_name]
+            move = True
+            for ix in self.intersections: # go through intersections
+                if robot.in_intersection(ix): # entering
+                    if not ix.can_enter(robot_name):
+                        move = False # cannot enter - stop now
+                    else:
+                        ix.enter(robot_name) # enter intersection
+                else: # leaving
+                    ix.leave(robot_name)
+            
+            # command robot to move or stop
+            self.get_logger().info(f'commanding robot {robot_name} to ' + ('move' if move else 'STOP'))
+            msg = String(data=robot_name)
+            if move:
+                self.pass_pub.publish(msg)
+            else:
+                self.stop_pub.publish(msg)
 
     def path_cb(self, data: Path):
         robot_name = data.header.frame_id # reused field, not up to spec
@@ -172,9 +196,9 @@ class CentralNavigationNode(Node):
 
         self.get_logger().info(f'received path of robot {robot_name} with {len(self.robots[robot_name].waypoints)} waypoint(s)')
         
-        self.find_collisions()
+        self.find_intersections()
     
-    def find_collisions(self):
+    def find_intersections(self):
         segments = []
         robot_seg_idx: dict[str, tuple[int, int]] = dict() # robot: (start, count)
         for robot_name in self.robots:
@@ -194,19 +218,30 @@ class CentralNavigationNode(Node):
             return None
 
         # save collision points
-        self.collision_points = []
+        intersections: list[Intersection] = []
         for (point, segment_idxs) in isect_segments_include_segments(segments):
             colliding_robots = set([robot_from_seg_idx(i) for i in segment_idxs])
             if len(colliding_robots) > 1: # count number of robots in collision zone
-                self.get_logger().info(f'collision point at {point}: {colliding_robots}')
-                self.collision_points.append(Intersection(point))
+                self.get_logger().info(f'intersection at {point}: {colliding_robots}')
+                intersections.append(Intersection(point))
+
+        # migrate intersections
+        old_intersections = filter(lambda ix: ix.occupied, self.intersections) # only consider occupied intersections
+        for ix in intersections:
+            old_ix = ix.closest_intersection(old_intersections)
+            if old_ix is not None:
+                old_ix_idx, old_ix = old_ix
+                ix_dist = ix.distance(old_ix.position)
+                if ix_dist <= 2 * IX_RADIUS: # intersections overlap
+                    ix.occupant = old_ix.occupant if self.robots[old_ix.occupant].in_intersection(ix) else None # existing robot in intersection gets priority (since it's moving already) - TODO
+        self.intersections = intersections
 
         self.publish_markers()
     
     def publish_markers(self):
         markers = [Marker(action=Marker.DELETEALL)] # delete all markers
         # send collision points out for visualisation
-        for point in self.collision_points:
+        for point in self.intersections:
             marker = point.marker
             marker.header.stamp = self.get_clock().now().to_msg()
             marker.id = len(markers)
